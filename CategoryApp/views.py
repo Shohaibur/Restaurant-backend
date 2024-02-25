78ea950ebc2b5b6e2e7380ce78ee5536b1d25c3c
Shohaibur <shoaibrochi9@gmail.com>
2024-02-25 13:21:19 +0600

    add template and diction

--- CategoryApp/views.py
+++ CategoryApp/views.py
@@ -4,13 +4,29 @@ from django.shortcuts import redirect
 # Create your views here.
 
 
-def index (request):
-  return HttpResponse("List of Category <br> <a href='breakfast/''>Breakfast</a> <br> <a href='lunch/''>Lunch</a> <br> <a href='dinner/''>Dinner</a> <br> <a href='dining/''>Home</a> ")
+# def index (request):
+#   return HttpResponse("List of Category <br> <a href='breakfast/''>Breakfast</a> <br> <a href='lunch/''>Lunch</a> <br> <a href='dinner/''>Dinner</a> <br> <a href='dining/''>Home</a> ")
+# def breakfast(request):
+#   return HttpResponse ("Welcome to Breakfast <br> <a href='/CategoryApp''>Back</a> ")
+# def lunch(request):
+#   return HttpResponse ("Welcome to Lunch <br> <a href='/CategoryApp''>Back</a> ")
+# def dinner(request):
+#   return HttpResponse ("Welcome to Dinner <br> <a href='/CategoryApp''>Back</a> ")
+# def redirect_to_dining_index(request):
+#   return redirect('DiningApp:index')
+
+def index(request):
+  diction ={}
+  return render (request, 'CategoryApp/index.html',context=diction)
 def breakfast(request):
-  return HttpResponse ("Welcome to Breakfast <br> <a href='/CategoryApp''>Back</a> ")
+  diction ={}
+  return render (request,'CategoryApp/breakfast.html',context=diction)
 def lunch(request):
-  return HttpResponse ("Welcome to Lunch <br> <a href='/CategoryApp''>Back</a> ")
+  diction ={}
+  return render(request,'CategoryApp/lunch.html',context=diction)
 def dinner(request):
-  return HttpResponse ("Welcome to Dinner <br> <a href='/CategoryApp''>Back</a> ")
+  diction={}
+  return render(request,'CategoryApp/dinner.html',context=diction)
 def redirect_to_dining_index(request):
   return redirect('DiningApp:index')
+  
--- DiningApp/views.py
+++ DiningApp/views.py
@@ -8,11 +8,23 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
+# def index(request):
+#   return HttpResponse("Welcome to Restaurant <br> <a href='/DiningApp/category/''> Category </a> <br> <a href='/DiningApp/contact/''>contact </a> <br> <a href='about/''>about </a>")
+# def contact(request):
+#   return HttpResponse("Contact Page <br> <a href='/DiningApp''>Home </a> <br> <a href='/DiningApp/about/''>about </a> ")
+# def about(request):
+#   return HttpResponse("About Page <br> <a href='/DiningApp''>Home </a> <br> <a href='/DiningApp/contact/''>contact </a> ")
+# def redirect_to_category_index(request):
+#   return redirect('CategoryApp:index')
+
 def index(request):
-  return HttpResponse("Welcome to Restaurant <br> <a href='/DiningApp/category/''> Category </a> <br> <a href='/DiningApp/contact/''>contact </a> <br> <a href='about/''>about </a>")
+  diction = { "text_1" : "Using Dictionary" }
+  return render(request, 'DiningApp/index.html', context=diction)
 def contact(request):
-  return HttpResponse("Contact Page <br> <a href='/DiningApp''>Home </a> <br> <a href='/DiningApp/about/''>about </a> ")
+  diction = {}
+  return render(request,'DiningApp/contact.html',context=diction)
 def about(request):
-  return HttpResponse("About Page <br> <a href='/DiningApp''>Home </a> <br> <a href='/DiningApp/contact/''>contact </a> ")
+  diction = {}
+  return render(request,'DiningApp/about.html',context=diction)
 def redirect_to_category_index(request):
   return redirect('CategoryApp:index')
